1fc141b0f11808be7e1b46763607d476348af3ff
Vini5577 <ugopessoa@gmail.com>
2024-04-20 14:10:19 -0300

    update model

--- src/main/java/com/api/contact/model/ContactModel.java
+++ src/main/java/com/api/contact/model/ContactModel.java
@@ -11,7 +11,13 @@ public class ContactModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
+    @Column(nullable = false)
     public String Name;
+
+    @Column(nullable = false, unique = true)
     public String Phone;
+
+    @Column(nullable = false)
     public Boolean IsActive;
 }
