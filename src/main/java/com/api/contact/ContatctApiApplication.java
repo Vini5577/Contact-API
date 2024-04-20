cf7f7c7e0638a7d0132f2bfd802ab21ecfb2d34a
Vini5577 <ugopessoa@gmail.com>
2024-04-20 13:52:01 -0300

    init project

---
+++ src/main/java/com/api/contact/ContatctApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatctApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatctApiApplication.class, args);
+	}
+
+}
